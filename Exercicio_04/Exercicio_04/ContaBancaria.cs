bb861666c0d92b0a2a90a6e78e56f7bff328613b
Fernando Henrique <fernandohjesus777@gmail.com>
2022-09-27 17:30:41 -0300

    Exercicio POO com encapsulamento

---
+++ Exercicio_04/Exercicio_04/ContaBancaria.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace Exercicio_04
+{
+     class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+
+        public Double Saldo { get; private set; }
+
+
+        //declaração do medoto consrutor
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        // Declaração do metodo de sobrecarga
+        public ContaBancaria( int numero, string titular, double saldo): this (numero, titular)  
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
